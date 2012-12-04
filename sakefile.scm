0945ef7393cd3f01387a66dc11bed98dd250e66c
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-12-04 19:59:03 +0100

    completed basic sakefile

--- sakefile.scm
+++ sakefile.scm
@@ -11,12 +11,19 @@
                                                       compiler-options: '(debug))))
             modules))
 
+(define-task clean ()
+  (sake:default-clean))
+
 (define-task install ()
   (for-each (lambda (m)
               (sake:install-compiled-module m)
               (sake:install-compiled-module m version: '(debug)))
             modules)
   (sake:install-system-sphere))
+
+(define-task uninstall ()
+  (sake:uninstall-system-sphere)
+  (delete-file prelude-system-path))
 
 (define-task all (compile install)
   'all)

9b79ce6798a63387bdf49fa34f7370fbc9020b95
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2012-12-25 17:55:04 +0100

    added SRFI-71: list/value procedures

--- src/algorithm/list-extra.scm
+++ src/algorithm/list-extra.scm
@@ -20,6 +20,34 @@
   (lambda (ls x)
     (append ls (list x))))
 
+;;! uncons (SRFI-71)
+(define (uncons pair)
+  (values (car pair) (cdr pair)))
+
+;;! uncons-2 (SRFI-71)
+(define (uncons-2 list)
+  (values (car list) (cadr list) (cddr list)))
+
+;;! uncons-3 (SRFI-71)
+(define (uncons-3 list)
+  (values (car list) (cadr list) (caddr list) (cdddr list)))
+
+;;! uncons-4 (SRFI-71)
+(define (uncons-4 list)
+  (values (car list) (cadr list) (caddr list) (cadddr list) (cddddr list)))
+
+;;! uncons-cons (SRFI-71)
+(define (uncons-cons alist)
+  (values (caar alist) (cdar alist) (cdr alist)))
+
+;;! unlist (SRFI-71)
+(define (unlist list)
+  (apply values list))
+
+;;! unvector (SRFI-71)
+(define (unvector vector)
+  (apply values (vector->list vector)))
+
 ;;! all cars and all cdrs
 (define (cars+cdrs ls)
   (call/cc

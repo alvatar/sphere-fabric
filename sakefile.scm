ac509736a874b03ebbe88969885b89bcaa6fc99a
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-12-28 16:38:33 +0100

    unified sakefiles

--- sakefile.scm
+++ sakefile.scm
@@ -19,17 +19,17 @@
   (for-each (lambda (m) (sake#compile-module m compiler-options: '(debug))) modules)
   (for-each sake#compile-module modules))
 
+(define-task post-compile ()
+  (for-each (lambda (m) (sake#make-module-available m versions: '(() (debug)))) modules))
+
+(define-task install ()
+  (sake#install-sphere-to-system))
+
 (define-task test ()
   (sake#test-all))
 
 (define-task clean ()
   (sake#default-clean))
 
-(define-task install ()
-  (for-each sake#install-compiled-module modules))
-
-(define-task force-install ()
-  (sake#install-sphere-to-system))
-
-(define-task all (compile install)
+(define-task all (compile post-compile)
   'all)

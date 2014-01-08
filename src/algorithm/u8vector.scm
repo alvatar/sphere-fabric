efd2070bbb05718fd48df99eb5d2d7a55f5518d3
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-01-08 18:46:47 +0100

    added u8vector functionality by Mikael Möre

--- src/algorithm/u8vector.scm
+++ src/algorithm/u8vector.scm
@@ -1,8 +1,6 @@
-;;; Copyright (c) 2005, Michael Sperber. All rights reserved.
-;;; Copyright (c) 2012-2014, Alvaro Castro-Castilla. All rights reserved.
-;;; Part of the API is implemented natively by Gambit
-
-;;;!!! SRFI-66 Octet Vectors
+;;; Copyright (C) 2008, 2011, 2012, 2013, 2014 Mikael More.
+;;; Copyright (c) 2005, Michael Sperber.
+;;; Copyright (c) 2012-2014, Alvaro Castro-Castilla.
 
 (cond-expand
  (optimize
@@ -10,6 +8,10 @@
  (debug
   (declare (safe) (debug) (debug-location) (debug-source) (debug-environments)))
  (else (void)))
+
+
+;;!! SRFI-66 Octet Vectors
+;; Part of the API is implemented natively by Gambit
 
 ;;! Copies data from octet vector source to octet vector target. Source-start,
 ;; target-start, and n must be non-negative exact integers that satisfy
@@ -70,3 +72,67 @@
               (cond ((< elt-1 elt-2) -1)
                     ((> elt-1 elt-2)  1)
                     (else (loop (+ i 1)))))))))))
+
+
+;; U8vector manipulation / handling
+;; Author: Mikael Möre
+;;
+;; (apply-u8vector-append u8vector-list)
+;;
+;; (u8vector-reverse u8v) => u8vector
+;; Makes a new u8vector with u8v's contents with reversed order on the contained bytes, and returns it.
+;;
+;; (u8vector-invert! u8v)
+;; Makes a new u8vector with u8v's contents with each byte inversed (i.e. (- 255 b)), and returns it.
+;;
+;; (u8vector-pad-to-length u8v to-length #!optional (pad-byte 0))
+;;
+;; (u8vector-xor u8v with-byte)
+(define (apply-u8vector-append u8vector-list)
+  (call-with-output-u8vector
+   '()
+   (lambda (output-port)
+     (let loop ((rest u8vector-list))
+       (if (not (null? rest))
+           (let ((v (car rest)))
+             (if (not (u8vector? v)) (error "apply-u8vector-append got non-u8vector argument" v))
+             (write-subu8vector v 0 (u8vector-length v) output-port)
+             (loop (cdr rest))))))))
+
+(define (u8vector-reverse v)
+  (let* ((l (u8vector-length v))
+         (r (make-u8vector l)))
+    (let loop ((src-idx 0) (target-idx (- l 1)))
+      (u8vector-set! r target-idx (u8vector-ref v src-idx))
+      (if (> target-idx 0)
+          (loop (+ src-idx 1) (- target-idx 1))))
+    r))
+
+(define (u8vector-invert! v)
+  (let loop ((i (u8vector-length v)))
+      (if (not (zero? i))
+          (let ((i (- i 1)))
+            (u8vector-set! v i (##fixnum.bitwise-xor
+                                (u8vector-ref v i)
+                                255))
+            (loop i)))))
+
+;; May return the originally passed |u8v| object reference.
+(define* (u8vector-pad-to-length u8v to-length (pad-byte 0))
+  (let ((u8v-length (u8vector-length u8v)))
+    (if (fx> to-length u8v-length)
+        (let ((new-u8v (make-u8vector to-length pad-byte)))
+          (subu8vector-move! u8v 0 u8v-length new-u8v 0)
+          new-u8v)
+        u8v)))
+
+(define (u8vector-xor u8v with-byte)
+  (let* ((length (u8vector-length u8v))
+         (new-u8v (make-u8vector length)))
+    (let loop ((at 0))
+      (if (##not (##eq? length at))
+          (begin
+            (##u8vector-set! new-u8v at (##fxxor (##u8vector-ref u8v at) with-byte))
+            (loop (##fx+ at 1)))))
+    new-u8v))
+

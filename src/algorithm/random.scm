53671ada7563207c566ce9425cc1f72531c16235
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2014-01-08 16:42:21 +0100

    turns out define* (and thus ##define) doesn't allow internal defines

--- src/algorithm/random.scm
+++ src/algorithm/random.scm
@@ -31,20 +31,20 @@
 ;;! Pick a random element and return also the list without that element
 ;; .parameter l input list
 ;; .parameter len (optional) give the length of the list, if known in advance
-;;! Pick a random element and return also the list without that element
-(define* (random-pick+rember l (len #f))
-  (define (random-pick+rember/length l len)
-    (let ((rnd (random-integer len)))
-      (let recur ((rest l)
-                  (i 0))
-        (if (fx= i rnd)
-            (values (car rest) (cdr rest))
-            (receive (head tail)
-                     (recur (cdr rest) (fx+ 1 i))
-                     (values head
-                             (cons (car rest)
-                                   tail)))))))
-  (random-pick+rember/length l (or len (length l))))
+(define* (random-extract l (len #f))
+  (let ((random-pick+rember/length
+         (lambda (l len)
+           (let ((rnd (random-integer len)))
+             (let recur ((rest l)
+                         (i 0))
+               (if (fx= i rnd)
+                   (values (car rest) (cdr rest))
+                   (receive (head tail)
+                            (recur (cdr rest) (fx+ 1 i))
+                            (values head
+                                    (cons (car rest)
+                                          tail)))))))))
+    (random-pick+rember/length l (or len (length l)))))
 
 ;;! Reservoir Sampling. Get a number of random elements from a list or stream
 (define random-sample 
